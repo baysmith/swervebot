271abc93c8f29d07bb897d65ce049c60ee3e1b19
Bradley Smith <bradley@baysmith.com>
2025-04-01 17:09:31 -0700

    Rewrote the InputSubsystem.
    Tested with the simulator using two keyboards connected as joysticks.

--- README.md
+++ README.md
@@ -7,11 +7,10 @@ Code for the ESHS P.O.T.A.T.O.E.S. swerve drive test platform.
 1. Temporarily disable feedForward mechanism (example code) to see what PID controller does, and vice versa. Test to see if we like how the feedForward mechanism works with the PID controller
 2. Socialize with multiple teams at competitions in order to learn more about robotics in general (buisness cards included)
 
-## Xbox controller
-1. Button B raises the lift
-2. Button A lowers the lift
-3. Drive forward and back with the left joystick's vertical channel.
-4. Turn with the right joystick's vertical channel.
+## Xbox controller or two joysticks
+1. Drive forward and back with the left joystick's vertical channel.
+2. Turn with the right joystick's vertical channel.
+3. Button 1 activates the coral intake.
+4. Button 2 raises the lift.
+5. Button 3 lowers the lift.
 
-## One joystick
-1. 
--- src/main/java/frc/robot/subsystems/InputSubsystem.java
+++ src/main/java/frc/robot/subsystems/InputSubsystem.java
@@ -5,9 +5,11 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -21,26 +23,15 @@ import frc.robot.Constants;
  * <p>On joystick support:</p>
  * <ul>
  *   <li>
- *     <p>The system supports up to two joysticks, known as the <em>main</em>
- *     and <em>secondary</em> joytsticks.  If there is only one joystick
- *     plugged into the system, it will be considered the main joystick; if
- *     there are two plugged in, whichever one was plugged in first will be
- *     main and the most recent one will be secondary.</p>
+ *     <p>The system requires two joysticks, known as the <em>main</em>
+ *     and <em>secondary</em> joysticks. Whichever one was plugged in first
+ *     will be main and the most recent one will be secondary.</p>
  *     <ul>
  *       <li> Any joysticks plugged in beyond the first two will be ignored.</li>
  *     </ul>
  *   </li>
  *   <li>
- *     <p>If only one joystick is plugged into the system, then the mappings
- *     to the driver channels are straightforward:</p>
- *     <ul>
- *       <li>X axis: {@link #getLeftRight() leftRight}</li>
- *       <li>Y axis: {@link #getForwardBack() forwardBack}</li>
- *       <li>Z axis (twisting the joystick): {@link #getTurn() turn}</li>
- *     </ul>
- *   </li>
- *   <li>
- *     <p>When two joysticks are plugged in, the mappings change as follows:</p>
+ *     <p>The mappings change as follows:</p>
  *     <ul>
  *       <li>Main joystick, X axis: {@link #getLeftRight() leftRight}</li>
  *       <li>Main joystick, Y axis: {@link #getForwardBack() forwardBack}</li>
@@ -53,13 +44,16 @@ import frc.robot.Constants;
  */
 public class InputSubsystem extends SubsystemBase {
 
+    final int CORAL_INTAKE_BUTTON = 1;
+    final int ARM_MOVEMENT_UP_BUTTON = 2;
+    final int ARM_MOVEMENT_DOWN_BUTTON = 3;
+
     // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
     private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
     private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
     private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
-    private XboxController xboxController;
-    private Joystick mainJoystick;
-    private Joystick secondaryJoystick;
+    private GenericHID mainController;
+    private GenericHID secondaryController;
 
     /**
      * How often should we look for new human input devices?
@@ -75,11 +69,11 @@ public class InputSubsystem extends SubsystemBase {
     int currentHeight;
 
     public InputSubsystem() {
-        xboxController = null;
-        mainJoystick = null;
-        secondaryJoystick = null;
+        mainController = null;
+        secondaryController = null;
         lastCheckTimeSeconds = Timer.getFPGATimestamp() - JOYSTICK_POLLING_INTERVAL_SECONDS; // Force initial check
         currentHeight = 0;
+        SmartDashboard.putData(this);
     }
 
     /**
@@ -116,13 +110,11 @@ public class InputSubsystem extends SubsystemBase {
      * </ul>
      */
     public double getLeftRight() {
-        double xboxLeftRight = (xboxController != null && xboxController.isConnected() ? xboxController.getLeftX() : 0);
-        double joystickLeftRight = (mainJoystick != null && mainJoystick.isConnected() ? mainJoystick.getX() : 0);
+        double joystickLeftRight = mainController != null ? mainController.getRawAxis(0) : 0;
 
         // If all devices are plugged in, then they'll all contribute so long as
         // a human is touching them.  This allows for pair driving.
-        double leftRight = MathUtil.applyDeadband(xboxLeftRight, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE) +
-                           MathUtil.applyDeadband(joystickLeftRight, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
+        double leftRight = MathUtil.applyDeadband(joystickLeftRight, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
 
         leftRight = MathUtil.clamp(leftRight, -1.0, 1.0);
 
@@ -143,11 +135,9 @@ public class InputSubsystem extends SubsystemBase {
      * </ul>
      */
     public double getForwardBack() {
-        double xboxForwardBack = (xboxController != null && xboxController.isConnected() ? xboxController.getLeftY() : 0);
-        double joystickForwardBack = (mainJoystick != null && mainJoystick.isConnected() ? mainJoystick.getY() : 0);
+        double joystickForwardBack = mainController != null ? mainController.getRawAxis(1) : 0;
 
-        double forwardBack = MathUtil.applyDeadband(xboxForwardBack, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE) +
-                             MathUtil.applyDeadband(joystickForwardBack, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
+        double forwardBack = MathUtil.applyDeadband(joystickForwardBack, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
 
         forwardBack = MathUtil.clamp(forwardBack, -1.0, 1.0);
         
@@ -169,12 +159,10 @@ public class InputSubsystem extends SubsystemBase {
      * </ul>
      */
     public double getTurn() {
-        double xboxTurn = (xboxController != null && xboxController.isConnected() ? xboxController.getRightX() : 0);
-        double joystickTurn = (secondaryJoystick != null && secondaryJoystick.isConnected() ? secondaryJoystick.getX() :
-                               (mainJoystick != null && mainJoystick.isConnected() ? mainJoystick.getZ() : 0));
+        double joystickTurn = secondaryController != null ? secondaryController.getRawAxis(0) : (
+                               mainController != null ? mainController.getRawAxis(2) : 0);
 
-        double turn = MathUtil.applyDeadband(xboxTurn, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE) +
-                      MathUtil.applyDeadband(joystickTurn, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
+        double turn = MathUtil.applyDeadband(joystickTurn, Constants.OperatorConstants.JOYSTICK_DEAD_ZONE);
 
         turn = MathUtil.clamp(turn, -1.0, 1.0);
 
@@ -198,31 +186,16 @@ public class InputSubsystem extends SubsystemBase {
             Constants.ArmConstants.DEFAULT_HEIGHT,
             Constants.ArmConstants.LIFT_HEIGHT_1,
             Constants.ArmConstants.LIFT_HEIGHT_2,
-            Constants.ArmConstants.LIFT_HEIGHT_3};
+            Constants.ArmConstants.LIFT_HEIGHT_3,
+        };
 
-
-        if(xboxController != null && xboxController.isConnected() == true) {
-            if(xboxController.getBButtonPressed() && currentHeight <= liftHeights.length) {
-                currentHeight++;
-            } else if(xboxController.getAButtonPressed() && currentHeight >= 0) {
-                currentHeight--;
+        if (secondaryController != null) {
+            if (secondaryController.getRawButtonPressed(ARM_MOVEMENT_UP_BUTTON) && currentHeight < liftHeights.length - 1) {
+                currentHeight += 1;
+            } else if (secondaryController.getRawButtonPressed(ARM_MOVEMENT_DOWN_BUTTON) && currentHeight > 0) {
+                currentHeight -= 1;
             }
         }
-
-        if(mainJoystick != null && mainJoystick.isConnected() && secondaryJoystick != null && secondaryJoystick.isConnected()) {
-            if(secondaryJoystick.getRawButtonPressed(6) && currentHeight <= liftHeights.length) {
-                currentHeight++;
-            } else if(secondaryJoystick.getRawButtonPressed(4) && currentHeight >= 0) {
-                currentHeight--;
-            }
-        } else if(mainJoystick != null && mainJoystick.isConnected()) {
-            if(mainJoystick.getRawButtonPressed(6) && currentHeight <= liftHeights.length) {
-                currentHeight++;
-            } else if(mainJoystick.getRawButtonPressed(4) && currentHeight >= 0) {
-                currentHeight--;
-            }
-        }
-
         return liftHeights[currentHeight];
     }
 
@@ -232,33 +205,14 @@ public class InputSubsystem extends SubsystemBase {
      * The speed of the lift is dependent on the constant LIFT_SPEED in Constants.java.
      */
     public double getArmMovement() {
-        if(xboxController != null && xboxController.isConnected() == true) {
-            if (xboxController.getYButton()) {
+        if (mainController != null) {
+            if (mainController.getRawButton(ARM_MOVEMENT_UP_BUTTON)) {
                 return Constants.ArmConstants.LIFT_SPEED;
-            } else if (xboxController.getAButton()) {
-                return Constants.ArmConstants.LIFT_SPEED * -1;
-            } else if (xboxController.getAButton()) {
-                return Constants.ArmConstants.LIFT_SPEED;
-            } else {
-                return 0;
+            } else if (mainController.getRawButton(ARM_MOVEMENT_DOWN_BUTTON)) {
+                return Constants.ArmConstants.LIFT_SPEED * -1.0;
             }
         }
-
-        if(mainJoystick != null && mainJoystick.isConnected() && secondaryJoystick != null && secondaryJoystick.isConnected()) {
-            if(secondaryJoystick.getRawButtonPressed(5)) {
-                return Constants.ArmConstants.LIFT_SPEED;
-            } else if(secondaryJoystick.getRawButtonPressed(3)) {
-                return Constants.ArmConstants.LIFT_SPEED * -1;
-            }
-        } else if(mainJoystick != null && mainJoystick.isConnected()) {
-            if(mainJoystick.getRawButtonPressed(5)) {
-                return Constants.ArmConstants.LIFT_SPEED;
-            } else if(mainJoystick.getRawButtonPressed(3)) {
-                return Constants.ArmConstants.LIFT_SPEED * -1;
-            }
-        }
-
-        return 0;
+        return 0.0;
     }
 
 
@@ -267,26 +221,12 @@ public class InputSubsystem extends SubsystemBase {
      * is pressing the right trigger/right bumper button.
      */
     public boolean isCoralIntakeActivated() {
-        if(xboxController != null &&xboxController.isConnected() == true) {
-            if(xboxController.getBButton()) {
-                return true;
-            } else {
-                return false;
-            }
+        if (mainController != null && mainController.getRawButton(CORAL_INTAKE_BUTTON)) {
+            return true;
         }
-
-        if(mainJoystick != null && mainJoystick.isConnected() && secondaryJoystick != null && secondaryJoystick.isConnected()) {
-            if(secondaryJoystick.getTrigger()) {
-                return true;
-            }
-        } else if(mainJoystick != null && mainJoystick.isConnected()) {
-            if(mainJoystick.getTrigger()) {
-                return true;
-            }
-        }
-
         return false;
     }
+
 
     /**
      * A function called every few seconds meant to detect when xbox controllers
@@ -296,115 +236,77 @@ public class InputSubsystem extends SubsystemBase {
      * function should only do so occasionally.</p>
      */
     private void controllerCheck() {
-        final int MAX_PORTS_TO_CHECK = 4;
-        for (int i = 0; i < MAX_PORTS_TO_CHECK; i++) {
-
-            try {
-                if (xboxController == null) {
-                    xboxController = new XboxController(i);
-                    System.out.printf("controllerCheck: Found xbox controller on port %d.\n", i);
-                    continue;
-                } else if (!xboxController.isConnected()) {
-                    xboxController = null;
-                    System.out.printf("controllerCheck: Xbox controller is disconnected.\n");
-                    continue;
-                }
-            } catch (Exception e) {
-                // Xbox controller is not connected on port i, not a big deal.
+        if (mainController != null) {
+            if (!mainController.isConnected()) {
+                System.out.println("Main controller disconnected.");
+                mainController = null;
             }
+        }
+        if (secondaryController != null) {
+            if (!secondaryController.isConnected()) {
+                System.out.println("Secondary controller disconnected.");
+                secondaryController = null;
+            }
+        }
 
-            // Can we still accept another joystick?
-            if (!mainJoystickFound() || !secondaryJoystickFound()) {
-
-                if ((mainJoystickFound() && mainJoystick.getPort() == i) ||
-                    (secondaryJoystickFound() && secondaryJoystick.getPort() == i)) {
-                    // We already know there's a working joystick on this
-                    // port.
-                    continue;
-                }
-
-                // We want to be able to support both 1- and 2-joystick
-                // scenarios.
+        final int MAX_PORTS_TO_CHECK = 5;
+        if (mainController == null) {
+            for (int port = 0; port < MAX_PORTS_TO_CHECK; port++) {
                 try {
-                    Joystick j = new Joystick(i);
-                    if (assignJoystick(j)) {
-                        continue;
+                    var xboxController = new XboxController(port);
+                    if (xboxController.isConnected()) {
+                        mainController = xboxController;
+                        System.out.printf("Main controller connected (xbox, port %d).\n", mainController.getPort());
+                        break;
                     }
                 } catch (Exception e) {
-                    // No joystick at port i, not a big deal.
+                    System.out.printf("xbox controller, port %d, exception %s\n", port, e.toString());
+                }
+
+                try {
+                    Joystick j = new Joystick(port);
+                    if (j.isConnected()) {
+                        mainController = j;
+                        System.out.printf("Main controller connected (joystick, port %d).\n", mainController.getPort());
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.printf("joystick, port %d, exception %s\n", port, e.toString());
                 }
             }
-            // TODO: Add NetworkTables support.
+        }
+        if (mainController != null && secondaryController == null) {
+            for (int port = 0; port < MAX_PORTS_TO_CHECK; port++) {
+                if (mainController.getPort() == port)
+                    continue;
+                try {
+                    Joystick j = new Joystick(port);
+                    if (j.isConnected()) {
+                        secondaryController = j;
+                        System.out.printf("Secondary controller connected (joystick, port %d).\n", port);
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.printf("joystick, port %d, exception %s\n", port, e.toString());
+                }
+            }
         }
     }
 
     /**
-     * A simple utility function that returns true if {@link #mainJoystick} is
+     * A simple utility function that returns true if {@link #mainController} is
      * usable.
      */
     private boolean mainJoystickFound() {
-        return mainJoystick != null && mainJoystick.isConnected();
+        return mainController != null && mainController.isConnected();
     }
 
     /**
      * A simple utility function that returns true if {@link
-     * #secondaryJoystick} is usable.
+     * #secondaryController} is usable.
      */
     private boolean secondaryJoystickFound() {
-        return secondaryJoystick != null && secondaryJoystick.isConnected();
-    }
-
-    /**
-     * Helper function for {@link #controllerCheck()}.  When a new joystick
-     * "comes online", this function determines where to assign the new
-     * joystick (if assigning is needed at all.)
-     *
-     * @param j a {@link Joystick} that has just been successfully
-     *          constructed.
-     * @return Returns true if the joystick was successfully assigned to the
-     *         main or secondary joystick and false if there was no need to
-     *         assign it.
-     */
-    private boolean assignJoystick(Joystick j) {
-        boolean success = true;
-        String message = "";
-
-        if (!mainJoystickFound()) {
-
-            if (!secondaryJoystickFound()) {
-                mainJoystick = j;
-                message = "assigned new joystick on port %d to main.\n".formatted(j.getPort());
-            } else {
-                mainJoystick = secondaryJoystick;
-                secondaryJoystick = j;
-                message = "assigned secondary to the main joystick and assigned new joystick on port %d to the secondary.\n".formatted(j.getPort());
-            }
-        } else { // Main joystick is connected.
-
-            if (!secondaryJoystickFound()) {
-                secondaryJoystick = j;
-                message = "assigned new joystick on port %d to secondary.\n".formatted(j.getPort());
-            } else {
-                // Already have two working joysticks, so 3rd joystick does nothing.
-                // Not even worth logging.
-                success = false;
-            }
-        }
-
-        if (success) {
-            final String prefix = "controllerCheck";
-
-            final String mainJoystickString = mainJoystick == null ? "Main joystick is missing" :
-                !mainJoystick.isConnected() ? "Main joystick on port %d was disconnected".formatted(mainJoystick.getPort()) :
-                "Main joystick is connected on port %d".formatted(mainJoystick.getPort());
-
-            final String secondaryJoystickString = secondaryJoystick == null ? "secondary joystick is missing" :
-                !secondaryJoystick.isConnected() ? "secondary joystick on port %d was disconnected".formatted(secondaryJoystick.getPort()) :
-                "secondary joystick is connected on port %d".formatted(secondaryJoystick.getPort());
-
-            System.out.printf("%s: %s and %s; %s.\n", prefix, mainJoystickString, secondaryJoystickString, message);
-        }
-        return success;
+        return secondaryController != null && secondaryController.isConnected();
     }
 
     /**
@@ -416,6 +318,9 @@ public class InputSubsystem extends SubsystemBase {
         builder.addDoubleProperty("Turn", this::getTurn, null);
         builder.addDoubleProperty("ForwardBack", this::getForwardBack, null);
         builder.addDoubleProperty("LeftRight", this::getLeftRight, null);
+        builder.addBooleanProperty("CoralIntake", this::isCoralIntakeActivated, null);
+        builder.addDoubleProperty("ArmMovement", this::getArmMovement, null);
+        builder.addDoubleProperty("LiftPosition", this::getDesiredPosition, null);
         // super.initSendable(builder);
     }
 
